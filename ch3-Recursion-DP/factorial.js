eb9393f8dea9fc08e336057ff4bdc985fa0a16ec
RizwanMushtaq <rizwanmushtaq15@gmail.com>
2022-02-17 15:26:26 +0100

    cartian Product algorithms

--- ch3-Recursion-DP/factorial.js
+++ ch3-Recursion-DP/factorial.js
@@ -4,11 +4,15 @@ function fact(number){
         return 1  //1
     }
     return number * fact(number-1) //1 
-    //we have 4 function call for fact of 4
-    //In each function call we have -> O(1)
-    // But we trigger multiple function calls => n function calls
-    // T = n * O(1) => O(n)
+    
 }
+// What is time complexity?
+// we have 4 function call for fact of 4
+// In each function call we have -> O(1)
+// But we trigger multiple function calls => n function calls
+// T = n * O(1) => O(n)
+// What is space complexity here?
+// Space Complexity: O(n) because a new value is created for every nested function call (the parameter received)
 
 
 // function fact(number){
@@ -18,6 +22,10 @@ function fact(number){
 //     }
 //     return result
 // }
+// What is time complexity here?
+// T => O(n)
+// What is spce complexity here?
+// Space Complexity: O(1) because we operate on one and same number, no new permanent value is created per iteration.
 
 console.log(fact(3))
 console.log(fact(4))
